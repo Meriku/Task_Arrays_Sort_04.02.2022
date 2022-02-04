1bc24cc4bd42d109111b8a459f08b98469f9a402
Myroslav <m.mirik.v@gmail.com>
2022-02-04 20:36:25 +0200

    Добавьте файлы проекта.

---
+++ Task_Arrays_Sort_04.02.2022/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Arrays_Sort_04._02._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ArrayLenght;                     
+            var rnd = new Random();
+            ConsoleKeyInfo UserAnswer;
+
+            Console.WriteLine("Введите количество чисел в массиве.");
+            ArrayLenght = int.Parse(Console.ReadLine());
+            int[] ArrayForSort = new int[ArrayLenght];
+
+            Console.WriteLine("Заполнить случайно? Y - да / N - нет.");
+            UserAnswer = Console.ReadKey();
+
+            if (UserAnswer.Key == ConsoleKey.N)
+            {           
+            for (int i = 0; i < ArrayLenght;)               // Заставляем пользователя ввести корректные значения.
+                {
+                Console.WriteLine("\nВведите число:");
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    ArrayForSort[i] = result;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректное число. Повторите попытку.");
+                }
+            }
+            }
+            else if (UserAnswer.Key == ConsoleKey.Y)        // Генерируем случайно. 
+            {
+                for (int i = 0; i < ArrayLenght; i++)
+                {
+                    ArrayForSort[i] = rnd.Next(0,1000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ответ не распознан. Массив будет содержать случайные значения. ");
+                for (int i = 0; i < ArrayLenght; i++)
+                {                    
+                    ArrayForSort[i] = rnd.Next(0, 1000);
+                }
+            }
+            
+            Console.WriteLine("\n\nВаш массив");
+            foreach (int item in ArrayForSort)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\n\nСортируем числа в массиве...");                 
+            foreach (int item in Sort(ArrayForSort))
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        public static int[] Sort (int[] arrayin)
+        {         
+            for (var j = 0; j < arrayin.Length; j++)
+            {
+                for (int i = j; i < arrayin.Length; i++)
+                {
+                    int temp = arrayin[j];
+                    if (temp < arrayin[i])              // Находим наибольшее значение
+                    {
+                        temp = arrayin[i];
+                        arrayin[i] = arrayin[j];
+                        arrayin[j] = temp;              // Переставляем его на место элемента j
+
+                    }
+                }      
+            }
+
+            return arrayin;
+            
+        }
+    }
+}
---
+++ Task_Arrays_Sort_04.02.2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task_Arrays_Sort_04.02.2022")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_Arrays_Sort_04.02.2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("91f4edb0-0ffc-4e88-9a34-2fb1d15bc31f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
